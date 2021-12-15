a1076838407455b35d6ef715d9efc9e6282f34f6
Vinicius <beneditoloura@gmail.com>
2021-12-15 20:40:36 -0300

    test: gutter game

--- src/game.spec.ts
+++ src/game.spec.ts
@@ -1,53 +1,11 @@
 import { Game } from './game'
 
-let g: Game = new Game()
-
-beforeEach(() => {
-  g = new Game()
-})
-
-describe('Bowling game', () => {
+describe('Bowling Game', () => {
   test('should score 0 for gutter game', () => {
-    rollMany(20, 0)
+    const g: Game = new Game()
+    for (let i = 0; i < 20; i++) {
+      g.roll(0)
+    }
     expect(g.score()).toBe(0)
   })
-
-  test('should score 20 for all ones game', () => {
-    rollMany(20, 1)
-    expect(g.score()).toBe(20)
-  })
-
-  test('should score 16 for a spare followed by a 3 ball', () => {
-    rollSpare()
-    g.roll(3)
-    expect(g.score()).toBe(16)
-  })
-
-  test('should score 24 for a strike followed by a 3 and a 4 ball', () => {
-    rollStrike()
-    g.roll(3)
-    g.roll(4)
-    rollMany(16, 0)
-    expect(g.score()).toBe(24)
-  })
-
-  test('should score 300 for perfect game', () => {
-    rollMany(12, 10)
-    expect(g.score()).toBe(300)
-  })
-
-  function rollStrike (): void {
-    g.roll(10)
-  }
-
-  function rollSpare (): void {
-    g.roll(5)
-    g.roll(5)
-  }
-
-  function rollMany (n: number, pins: number): void {
-    for (let i = 0; i < n; i++) {
-      g.roll(pins)
-    }
-  }
 })
