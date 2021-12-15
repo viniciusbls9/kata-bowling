f18b1bb1cf7ad5f4a8b6e41020d4a60e4351307b
Vinicius <beneditoloura@gmail.com>
2021-12-15 20:40:24 -0300

    feat: gutter game

--- src/game.ts
+++ src/game.ts
@@ -1,47 +1,9 @@
 export class Game {
-  private rolls: number[] = Array(21).fill(0)
-  private currentRoll: number = 0
+  roll (pins: number): void {
+
+  }
 
   score (): number {
-    let score = 0
-    let frameIndex = 0
-    for (let frame = 0; frame < 10; frame++) {
-      if (this.isStrike(frameIndex)) {
-        score += 10 + this.strikeBonus(frameIndex)
-        frameIndex++
-      } else if (this.isSpare(frameIndex)) {
-        score += 10 + this.spareBonus(frameIndex)
-        frameIndex += 2
-      } else {
-        score += this.sumOfBallsInFrame(frameIndex)
-        frameIndex += 2
-      }
-    }
-    return score
-  }
-
-  private sumOfBallsInFrame (frameIndex: number): number {
-    return this.rolls[frameIndex] + this.rolls[frameIndex + 1]
-  }
-
-  private strikeBonus (frameIndex: number): number {
-    return this.rolls[frameIndex + 1] +
-           this.rolls[frameIndex + 2]
-  }
-
-  private spareBonus (frameIndex: number): number {
-    return this.rolls[frameIndex + 2]
-  }
-
-  private isStrike (frameIndex: number): boolean {
-    return this.rolls[frameIndex] === 10
-  }
-
-  private isSpare (frameIndex: number): boolean {
-    return this.rolls[frameIndex] + this.rolls[frameIndex + 1] === 10
-  }
-
-  roll (pins: number): void {
-    this.rolls[this.currentRoll++] = pins
+    return 0
   }
 }
